c3c876c9e56a2d4ac2deaaf5120c80338598ff6f
Cédric Louguet <cedric.louguet@gmail.com>
2021-11-20 11:07:46 +0100

    creation of API (localy first)

---
+++ FlaskAPI/app.py
@@ -0,0 +1,35 @@
+import flask
+from flask import Flask, jsonify, request
+import json
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+def load_models():
+    file_name = "models/selected_model_smote.sav"
+    with open(file_name, 'rb') as pickled:
+        model = pickle.load(pickled)
+    return model
+
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    
+    data = pd.read_csv('../Data/sample_to_train.csv')
+    
+    # stub input features
+    x = np.array(data.drop('TARGET', axis=1).iloc[0]).reshape(1,-1)
+    
+    # load model
+    model = load_models()
+    prediction = model.predict_proba(x)[0][1]
+    response = json.dumps({'response': float(prediction)})
+    return response, 200
+
+if __name__ == '__main__':
+
+    application.run(debug=True)
+
+
