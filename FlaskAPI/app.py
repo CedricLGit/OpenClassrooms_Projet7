83051e600490fd026dd5af83d439fba280b55f02
Cédric Louguet <cedric.louguet@gmail.com>
2021-12-11 07:54:24 +0100

    finalisation dashboard et api + deploiement api heroku et streamlit

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -8,7 +8,7 @@ import numpy as np
 app = Flask(__name__)
 
 def load_models():
-    file_name = "Data_API/selected_model_smote.sav"
+    file_name = "Data_API/model_smote_v3.sav"
     with open(file_name, 'rb') as pickled:
         model = pickle.load(pickled)
     return model
@@ -17,17 +17,15 @@ def load_models():
 @app.route('/predict', methods=['GET'])
 def predict():
     
-    data = pd.read_csv('Data_API/sample_to_train.csv')
-    
-    # stub input features
-    # x = np.array(data.drop('TARGET', axis=1).iloc[0]).reshape(1,-1)
-    id = int(request.args['client_id'])
-    x_in = np.array(data.drop('TARGET', axis=1)[data.SK_ID_CURR == id]).reshape(1,-1)
+    # retrieve input from request of dashboard
+    data_input = request.get_json()
+    x = data_input['input']
+    x_in = np.array(x)
     
     # load model
     model = load_models()
     prediction = model.predict_proba(x_in)[0][1]
-    response = json.dumps(float(prediction))
+    response = json.dumps(round(float(prediction),3))
     return response, 200
 
 if __name__ == '__main__':
--- streamlit/functions.py
+++ streamlit/functions.py
@@ -9,11 +9,31 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import plotly.graph_objs as go
 import plotly.figure_factory as ff
 from plotly.subplots import make_subplots
 from sklearn.preprocessing import LabelEncoder
+import pickle
+import shap
+import matplotlib.pyplot as plt
+import xgboost as xgb
 
+
+@st.cache
+def reduce_memory_usage(df):
+    
+    '''
+    convertir les dtype afin de réduire l\'utilisation de la mémoire
+    '''
+    
+    for col, col_type in df.dtypes.iteritems():
+        if col_type == 'object':
+            df[col] = df[col].astype('category')
+        elif col_type == 'float64':
+            df[col] = df[col].astype('float32')
+        elif col_type == 'int64':
+            df[col] = df[col].astype('int32')
+        
+    return df
 
 @st.cache
 def preprocess(df):
@@ -31,10 +51,7 @@ def preprocess(df):
             le.transform(df[col])
             
     # Encoder en 'OneHot' les variables de type category
-    df = pd.get_dummies(df)
-
-    # Il y a des valeurs anormales pour 'DAYS_EMPLOYED' que l'on va supprimer  
-    df['DAYS_EMPLOYED'].replace(365243, np.nan, inplace=True)    
+    df = pd.get_dummies(df)    
     
     return df
 
@@ -47,10 +64,12 @@ def cleaning(df):
     
     df_clean = df.dropna(axis=1, thresh=perc).copy()
     
+    # Il y a des valeurs anormales pour 'DAYS_EMPLOYED' que l'on va supprimer  
+    df['DAYS_EMPLOYED'].replace(365243, np.nan, inplace=True)
+    
     # Imputer les nans
     
     numdata = df_clean.select_dtypes('number')
-    datcol = df_clean.select_dtypes('number')
     
     for col in numdata.columns[numdata.isna().any()]:
         df_clean[col].fillna(df_clean[col].mean(), inplace = True)
@@ -62,14 +81,16 @@ def countplot(dataframe, x):
     
     dtype = dataframe[x].dtype
     dataframe[x] = dataframe[x].astype(str) # Convertir en string pour une meilleure visualisation
-    
-    if x == 'TARGET':
-        titre = 'Nombre de prêts selon le défault de remboursement'
 
     fig = px.histogram(dataframe,
                        x=x,
-                       color='TARGET',
-                       title=titre)
+                       color='TARGET')
+    fig.update_layout(yaxis_title='',
+                      title={'text': 'Nombre de clients par classe',
+                             'x': 0.5,
+                             'y': 0.03,
+                             'xanchor': 'center',
+                             'yanchor': 'bottom'})
     
     dataframe[x] = dataframe[x].astype(dtype) # Reconvertir au format d'origine
     
@@ -79,37 +100,113 @@ def countplot(dataframe, x):
 def correlation(dataframe):
     
     corr = dataframe.corr()
-    top10_corr = abs(corr.TARGET.drop('TARGET')).sort_values(ascending=False).head(10)
+    top10 = abs(corr.TARGET.drop('TARGET')).sort_values(ascending=False).head(10).index
+    top10_corr = corr.TARGET.drop('TARGET')[top10].sort_values(ascending=False)
     
     return top10_corr
 
 @st.cache
-def distribution(dataframe, features):
+def distribution(dataframe, features, comparaison=False, id_client=''):
     
-    fig = make_subplots(rows=(len(features)//2)+1, cols=2)
+    nb_row = (len(features)//2)+1
+    
+    fig = make_subplots(rows=nb_row, cols=2,
+                        subplot_titles=['{}'.format(feat) for feat in features],
+                        vertical_spacing=0.15)
+    leg_num = False
+    leg_cat = False
     
     for i,feat in enumerate(features):
         
         if i%2 == 0:
             col=1
         else:
-            col=2           
+            col=2
+            
+        if dataframe[feat].dtype == 'category':
+            
+            fig2=px.histogram(dataframe,
+                              x=feat,
+                              color='TARGET')
+            fig2.update_xaxes(tickangle=45, type='category',
+                              showticklabels=True)
+            
+            if leg_cat:
+                fig2['data'][0]['showlegend'] = False
+                fig2['data'][1]['showlegend'] = False
+                
+            else:
+                leg_cat=True
+                
+            fig.add_trace(fig2['data'][0],
+                          row=(i//2)+1, col=col)
+            fig.add_trace(fig2['data'][1],
+                          row=(i//2)+1, col=col)
+            
+            
+        else:
+            if feat == 'DAYS_EMPLOYED':
+                data0 = dataframe[(dataframe.TARGET==0) & (dataframe[feat]>-10000)][feat]
+                data1 = dataframe[(dataframe.TARGET==1) & (dataframe[feat]>-10000)][feat]
+            
+            else:    
+                data0 = dataframe[dataframe.TARGET==0][feat]
+                data1 = dataframe[dataframe.TARGET==1][feat]
+            fig2 = ff.create_distplot([data0, data1],
+                                      ['Prêt sans défault de paiement', 'Prêt avec défault de paiement'],
+                                      show_hist=False, show_rug=False)
+            if leg_num:
+                fig2['data'][0]['showlegend'] = False
+                fig2['data'][1]['showlegend'] = False
+            else:
+                leg_num=True
         
-        data0 = dataframe[dataframe.TARGET==0][feat]
-        data1 = dataframe[dataframe.TARGET==1][feat]
+            fig.add_trace(fig2['data'][0],
+                          row=(i//2)+1, col=col)
+            fig.add_trace(fig2['data'][1],
+                          row=(i//2)+1, col=col)
         
-        fig2 = ff.create_distplot([data0, data1],
-                                  ['Prêt sans défault de paiement', 'Prêt avec défault de paiement'],
-                                  show_hist=False, show_rug=False)
+        if comparaison == True:
+            
+            fig.add_vline(x=dataframe[dataframe.SK_ID_CURR == id_client][feat].values[0],
+                          line_dash="dash",
+                          row=(i//2)+1, col=col)
         
-        fig.add_trace(fig2['data'][0],
-                      row=(i//2)+1, col=col)
-        fig.add_trace(fig2['data'][1],
-                      row=(i//2)+1, col=col)
-        
-    fig.update_layout(showlegend=False,
-                      title_text='Distribution des variables selectionnées')
+    fig.update_layout(height=400*nb_row)
     
     return fig
 
-0%2
+@st.cache
+def explanation(df_train):
+    
+    model = pickle.load(open('model_smote_v3.sav', 'rb'))
+    explainer = shap.TreeExplainer(model['clf'])
+    shap_values = explainer.shap_values(df_train)
+
+    return explainer.expected_value, shap_values
+
+@st.cache
+def feature_imp(type_feat):
+    
+    model = pickle.load(open('model_smote_v3.sav', 'rb'))
+    
+    if type_feat == 'xgb':
+        
+        fig = xgb.plot_importance(model['clf'],
+                                  max_num_features=10,
+                                  importance_type='gain')
+    
+    return fig
+
+@st.cache
+def recup_col(dataframe, liste_col):
+    new_col = []
+    for col in liste_col:
+        if col not in dataframe.columns:
+            col = col.replace('_'+col.split('_')[-1], '')
+            new_col.append(col)
+        else:
+            new_col.append(col)
+    new_col = list(set(new_col))
+    
+    return new_col
